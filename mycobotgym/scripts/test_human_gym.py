5a091766a366b50014cf5a2aebd0a9ca3f4b4c0c
Matin Moezzi <matin.moezzi@outlook.com>
2023-02-15 00:05:51 -0500

    bug fixes

--- mycobotgym/scripts/test_human_gym.py
+++ mycobotgym/scripts/test_human_gym.py
@@ -6,25 +6,28 @@ import mycobotgym.envs
 from gymnasium_robotics.utils import mujoco_utils
 
 
+def print_contacts(model, data):
+    for geom1, geom2 in zip(data.contact.geom1, data.contact.geom2):
+        body1_id = model.geom_bodyid[geom1]
+        body2_id = model.geom_bodyid[geom2]
+        body1_name = mujoco.mj_id2name(
+            model, mujoco.mjtObj.mjOBJ_BODY, body1_id)
+        body2_name = mujoco.mj_id2name(
+            model, mujoco.mjtObj.mjOBJ_BODY, body2_id)
+        print(body1_name, body2_name)
+
+
 # env = gym.make("FetchPickAndPlace-v2", render_mode="human")
 env = gym.make("PickAndPlaceEnv-v0", render_mode="human")
 observation = env.reset(seed=42)
-for geom1, geom2 in zip(env.data.contact.geom1, env.data.contact.geom2):
-    body1_id = env.model.geom_bodyid[geom1]
-    body2_id = env.model.geom_bodyid[geom2]
-    body1_name = mujoco.mj_id2name(
-        env.model, mujoco.mjtObj.mjOBJ_BODY, body1_id)
-    body2_name = mujoco.mj_id2name(
-        env.model, mujoco.mjtObj.mjOBJ_BODY, body2_id)
-    print(body1_name, body2_name)
-for i in range(1000):
-    if i % 10 == 0:
+for i in range(10000):
+    action = env.action_space.sample()  # User-defined policy function
+    observation, reward, terminated, truncated, info = env.step(action)
+    if i % 1000 == 0:
         env.reset()
-    env.render()
 
 # action = env.action_space.sample()  # User-defined policy function
 # observation, reward, terminated, truncated, info = env.step(action)
-# env.step(action)
 # if i % 100 == 0:
 #         observation = env.reset()
 #     # frame = env.render(mode="rgb_array")
