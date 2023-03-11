b77b6f9120f0ae1faa48b584c0ca7fc9b619d84e
Matin Moezzi <matin.moezzi@outlook.com>
2023-03-10 21:04:25 -0500

    add reward shaping

--- mycobotgym/envs/__init__.py
+++ mycobotgym/envs/__init__.py
@@ -1,12 +1,13 @@
 import itertools
 import gymnasium
 
-for reward_type, has_object in itertools.product(["dense", "sparse"], [True, False]):
-    reward = "Dense" if reward_type == "dense" else "Sparse"
+REWARD = {"dense": "Dense", "sparse": "Sparse",
+          "reward_shaping": "RewardShaping"}
+for reward_type, has_object in itertools.product(["dense", "sparse", "reward_shaping"], [True, False]):
     kwargs = {
         "reward_type": reward_type,
         "has_object": has_object
     }
     name = "PickAndPlaceEnv" if has_object else "ReachObjectEnv"
-    gymnasium.register(f"{name}-{reward}-v0",
-                       entry_point="mycobotgym.envs.pick_and_place:PickAndPlaceEnv", kwargs=kwargs, max_episode_steps=100)
+    gymnasium.register(f"{name}-{REWARD[reward_type]}-v0",
+                       entry_point="mycobotgym.envs.pick_and_place:PickAndPlaceEnv", kwargs=kwargs, max_episode_steps=500)
