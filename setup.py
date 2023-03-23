75172378d44fe8e2a1e75eeb50ad53ee93972c3b
Matin Moezzi <matin.moezzi@outlook.com>
2023-03-23 01:25:03 -0400

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import find_packages, setup
 
 setup(name="MyCobotGym", version="1.0", author="Matin Moezzi", author_email="matin.moezzi@mail.utoronto.ca",
-      description="Reinforcement Learning Framework for Robotic Arm Tasks using MyCobot", packages=find_packages(), python_requires=">=3.10")
+      description="Reinforcement Learning Framework for Robotic Arm Tasks using MyCobot", packages=find_packages(), python_requires=">=3.9")
