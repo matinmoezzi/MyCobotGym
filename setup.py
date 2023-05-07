682aae47520e6cb70192c2393e21e40f86c3a8ef
Matin Moezzi <matin.moezzi@outlook.com>
2023-05-07 11:43:00 -0400

    fix bug

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email="matin.moezzi@mail.utoronto.ca",
     description="Reinforcement Learning Framework for Robotic Arm Tasks using MyCobot",
     packages=find_packages(),
-    install_requires=["gymnasium_robotics==1.2.0", "mujoco=2.3.2"],
+    install_requires=["gymnasium_robotics==1.2.0", "mujoco==2.3.2"],
     python_requires=">=3.9",
 )
