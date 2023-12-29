aaa5fdbe627c5661ed0ad33223e1e0f4cd8427fa
Matin Moezzi <matin.moezzi@outlook.com>
2023-12-29 14:10:57 -0500

    fix mycobot reward shaping bug

--- mycobotgym/envs/mycobot.py
+++ mycobotgym/envs/mycobot.py
@@ -31,11 +31,27 @@ def limit_obj_loc(pos):
 
 
 class MyCobotEnv(MujocoEnv):
-    metadata = {"render_modes": [
-        "human", "rgb_array", "depth_array"], "render_fps": 25}
+    metadata = {"render_modes": ["human", "rgb_array", "depth_array"], "render_fps": 25}
 
-    def __init__(self, model_path: str = "./assets/mycobot280.xml", has_object=True, block_gripper=False, control_steps=5, controller_type: Literal['mocap', 'IK', 'joint', 'delta_joint'] = 'IK', obj_range: float = 0.1, target_range: float = 0.1, target_offset: float = 0.0, target_in_the_air=True, distance_threshold=0.05, initial_qpos: dict = {}, fetch_env: bool = False, reward_type="sparse", frame_skip: int = 20, default_camera_config: dict = DEFAULT_CAMERA_CONFIG, **kwargs) -> None:
-
+    def __init__(
+        self,
+        model_path: str = "./assets/mycobot280.xml",
+        has_object=True,
+        block_gripper=False,
+        control_steps=5,
+        controller_type: Literal["mocap", "IK", "joint", "delta_joint"] = "IK",
+        obj_range: float = 0.1,
+        target_range: float = 0.1,
+        target_offset: float = 0.0,
+        target_in_the_air=True,
+        distance_threshold=0.05,
+        initial_qpos: dict = {},
+        fetch_env: bool = False,
+        reward_type="sparse",
+        frame_skip: int = 20,
+        default_camera_config: dict = DEFAULT_CAMERA_CONFIG,
+        **kwargs
+    ) -> None:
         self.block_gripper = block_gripper
         self.has_object = has_object
         self.target_in_the_air = target_in_the_air
@@ -64,7 +80,7 @@ class MyCobotEnv(MujocoEnv):
             frame_skip,
             observation_space,
             default_camera_config=default_camera_config,
-            ** kwargs,
+            **kwargs,
         )
         self.model_names = MujocoModelNames(self.model)
 
@@ -75,16 +91,16 @@ class MyCobotEnv(MujocoEnv):
         self.init_ctrl = self.data.ctrl.ravel().copy()
 
         self.controller = None
-        if self.controller_type == 'IK':
+        if self.controller_type == "IK":
             self.controller = IKController(self.model, self.data)
             if self.fetch_env:
-                action_size = 4    # 3 translation + 1 gripper
+                action_size = 4  # 3 translation + 1 gripper
             else:
                 # 3 translation + 3 rotation (euler) + 1 gripper
                 action_size = 7
-        elif self.controller_type in ['joint', 'delta_joint']:
+        elif self.controller_type in ["joint", "delta_joint"]:
             action_size = 7  # 6 joint positions + 1 gripper
-        elif self.controller_type == 'mocap':
+        elif self.controller_type == "mocap":
             if self.fetch_env:
                 action_size = 4  # 3 mocap cartesian position + 1 gripper
             else:
@@ -117,13 +133,12 @@ class MyCobotEnv(MujocoEnv):
 
     def step(self, action):
         action = np.clip(action, self.action_space.low, self.action_space.high)
-        if self.controller_type == 'IK':
+        if self.controller_type == "IK":
             assert self.controller is not None
             current_eef_pose = self.data.site_xpos[
                 self.model_names.site_name2id["EEF"]
             ].copy()
-            target_eef_pose = current_eef_pose + \
-                action[:3] * MAX_CARTESIAN_DISPLACEMENT
+            target_eef_pose = current_eef_pose + action[:3] * MAX_CARTESIAN_DISPLACEMENT
             if self.fetch_env:
                 quat_rot = euler2quat(np.zeros(3) * MAX_ROTATION_DISPLACEMENT)
             else:
@@ -136,18 +151,15 @@ class MyCobotEnv(MujocoEnv):
             )  # desired end effector orientation in quaternions
             mujoco.mju_mat2Quat(
                 current_eef_quat,
-                self.data.site_xmat[self.model_names.site_name2id["EEF"]].copy(
-                ),
+                self.data.site_xmat[self.model_names.site_name2id["EEF"]].copy(),
             )
-            mujoco.mju_mulQuat(target_orientation,
-                               quat_rot, current_eef_quat)
+            mujoco.mju_mulQuat(target_orientation, quat_rot, current_eef_quat)
 
             ctrl_action = np.zeros(7)
 
             # Denormalize gripper action
             ctrl_action[-1] = (
-                self.actuation_center[-1] +
-                action[-1] * self.actuation_range[-1]
+                self.actuation_center[-1] + action[-1] * self.actuation_range[-1]
             )
 
             for _ in range(self.control_steps):
@@ -164,32 +176,29 @@ class MyCobotEnv(MujocoEnv):
         elif self.controller_type == "mocap":
             mocap_action = np.zeros(7)
             mocap_action[:3] = action[:3] * 0.04
-            grip_tcp_quat = self.data.xquat[self.model_names.body_name2id["gripper_tcp"]]
+            grip_tcp_quat = self.data.xquat[
+                self.model_names.body_name2id["gripper_tcp"]
+            ]
             if self.fetch_env:
-                mocap_action[3:7] = np.array(
-                    [0.5, -0.5, -0.5, 0.5])
+                mocap_action[3:7] = np.array([0.5, -0.5, -0.5, 0.5])
             else:
                 mocap_action[3:7] = action[3:7]
             mocap_action[3:7] -= grip_tcp_quat
             mujoco_utils.mocap_set_action(self.model, self.data, mocap_action)
             self.data.ctrl[-1] = (
-                self.actuation_center[-1] +
-                action[-1] * self.actuation_range[-1]
+                self.actuation_center[-1] + action[-1] * self.actuation_range[-1]
             )
-            mujoco.mj_step(self.model, self.data,
-                           nstep=self.frame_skip)
-        elif self.controller_type == 'joint':
+            mujoco.mj_step(self.model, self.data, nstep=self.frame_skip)
+        elif self.controller_type == "joint":
             # Denormalize the input action from [-1, 1] range to the each actuators control range
-            action = self.actuation_center + \
-                action * self.actuation_range
+            action = self.actuation_center + action * self.actuation_range
             if self.fetch_env:
                 action[4:6] = self.init_ctrl[4:6]
             self.do_simulation(action, self.frame_skip)
-        elif self.controller_type == 'delta_joint':
+        elif self.controller_type == "delta_joint":
             mujoco.mj_forward(self.model, self.data)
             self.data.ctrl[-1] = (
-                self.actuation_center[-1] +
-                action[-1] * self.actuation_range[-1]
+                self.actuation_center[-1] + action[-1] * self.actuation_range[-1]
             )
             self.data.ctrl[:-1] += action[:-1] * MAX_JOINT_DISPLACEMENT
             if self.fetch_env:
@@ -200,25 +209,39 @@ class MyCobotEnv(MujocoEnv):
 
         obs = self._get_obs()
 
-        info = {
-            "is_success": self._is_success(obs["achieved_goal"], self.goal)}
+        info = {"is_success": self._is_success(obs["achieved_goal"], self.goal)}
         reward = self.compute_reward(obs["achieved_goal"], self.goal, info)
         terminated = self.compute_terminated(
-            obs["achieved_goal"], obs["desired_goal"], info)
+            obs["achieved_goal"], obs["desired_goal"], info
+        )
         truncated = self.compute_truncated(
-            obs["achieved_goal"], obs["desired_goal"], info)
+            obs["achieved_goal"], obs["desired_goal"], info
+        )
 
         if self.render_mode == "human":
             self.mujoco_renderer.viewer.add_overlay(
-                mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT, "is_success", str(info["is_success"]))
+                mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT,
+                "is_success",
+                str(info["is_success"]),
+            )
             if self.has_object:
-                self.mujoco_renderer.viewer.add_overlay(mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT, "distance_object_target", "%.3f" %
-                                                        goal_distance(obs["achieved_goal"], self.goal))
-                self.mujoco_renderer.viewer.add_overlay(mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT, "distance_gripper_object", "%.3f" %
-                                                        goal_distance(obs["achieved_goal"], obs["observation"][:3]))
+                self.mujoco_renderer.viewer.add_overlay(
+                    mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT,
+                    "distance_object_target",
+                    "%.3f" % goal_distance(obs["achieved_goal"], self.goal),
+                )
+                self.mujoco_renderer.viewer.add_overlay(
+                    mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT,
+                    "distance_gripper_object",
+                    "%.3f"
+                    % goal_distance(obs["achieved_goal"], obs["observation"][:3]),
+                )
             else:
-                self.mujoco_renderer.viewer.add_overlay(mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT, "distance_gripper_target", "%.3f" %
-                                                        goal_distance(obs["achieved_goal"], self.goal))
+                self.mujoco_renderer.viewer.add_overlay(
+                    mujoco.mjtGridPos.mjGRID_BOTTOMRIGHT,
+                    "distance_gripper_target",
+                    "%.3f" % goal_distance(obs["achieved_goal"], self.goal),
+                )
             self.render()
         return obs, reward, terminated, truncated, info
 
@@ -317,8 +340,7 @@ class MyCobotEnv(MujocoEnv):
         elif self.reward_type == "dense":
             return -d
         elif self.reward_type == "reward_shaping":
-            grip_pos = mujoco_utils.get_site_xpos(self.model, self.data, "EEF")
-            reward = int(self._is_success(grip_pos, achieved_goal, goal))
+            reward = int(self._is_success(achieved_goal, goal))
             reward += max(self.stage_rewards())
             return reward
 
@@ -327,15 +349,11 @@ class MyCobotEnv(MujocoEnv):
             mujoco_utils.set_joint_qpos(
                 self.model, self.data, "right_finger_joint", 0.0
             )
-            mujoco_utils.set_joint_qpos(
-                self.model, self.data, "left_finger_joint", 0.0
-            )
+            mujoco_utils.set_joint_qpos(self.model, self.data, "left_finger_joint", 0.0)
             mujoco.mj_forward(self.model, self.data)
 
     def _render_callback(self):
-        site_id = mujoco.mj_name2id(
-            self.model, mujoco.mjtObj.mjOBJ_SITE, "target0"
-        )
+        site_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, "target0")
         self.model.site_pos[site_id] = self.goal
         mujoco.mj_forward(self.model, self.data)
 
@@ -345,33 +363,26 @@ class MyCobotEnv(MujocoEnv):
 
     def generate_mujoco_observations(self):
         # positions
-        grip_pos = mujoco_utils.get_site_xpos(
-            self.model, self.data, "EEF")
+        grip_pos = mujoco_utils.get_site_xpos(self.model, self.data, "EEF")
 
         dt = self.frame_skip * self.model.opt.timestep
-        grip_velp = (
-            mujoco_utils.get_site_xvelp(
-                self.model, self.data, "EEF") * dt
-        )
+        grip_velp = mujoco_utils.get_site_xvelp(self.model, self.data, "EEF") * dt
 
         robot_qpos, robot_qvel = mujoco_utils.robot_get_obs(
             self.model, self.data, self.model_names.joint_names
         )
         if self.has_object:
-            object_pos = mujoco_utils.get_site_xpos(
-                self.model, self.data, "object0")
+            object_pos = mujoco_utils.get_site_xpos(self.model, self.data, "object0")
             # rotations
             object_rot = rotations.mat2euler(
                 mujoco_utils.get_site_xmat(self.model, self.data, "object0")
             )
             # velocities
             object_velp = (
-                mujoco_utils.get_site_xvelp(
-                    self.model, self.data, "object0") * dt
+                mujoco_utils.get_site_xvelp(self.model, self.data, "object0") * dt
             )
             object_velr = (
-                mujoco_utils.get_site_xvelr(
-                    self.model, self.data, "object0") * dt
+                mujoco_utils.get_site_xvelr(self.model, self.data, "object0") * dt
             )
             # gripper state
             object_rel_pos = object_pos - grip_pos
@@ -410,7 +421,9 @@ class MyCobotEnv(MujocoEnv):
 
     def _check_contact(self, gripper_id, object_id):
         for contact in self.data.contact:
-            if (gripper_id == contact.geom1 and object_id == contact.geom2) or (object_id == contact.geom1 and gripper_id == contact.geom2):
+            if (gripper_id == contact.geom1 and object_id == contact.geom2) or (
+                object_id == contact.geom1 and gripper_id == contact.geom2
+            ):
                 return True
         return False
 
@@ -431,37 +444,41 @@ class MyCobotEnv(MujocoEnv):
         grasp_mult = 0.35
         lift_mult = 0.7
 
-        grip_pos = mujoco_utils.get_site_xpos(
-            self.model, self.data, "EEF")
-        object_pos = mujoco_utils.get_site_xpos(
-            self.model, self.data, "object0")
-        target_pos = mujoco_utils.get_site_xpos(
-            self.model, self.data, "target0")
+        grip_pos = mujoco_utils.get_site_xpos(self.model, self.data, "EEF")
+        object_pos = mujoco_utils.get_site_xpos(self.model, self.data, "object0")
+        target_pos = mujoco_utils.get_site_xpos(self.model, self.data, "target0")
 
         r_reach = 0.0
-        r_reach = (
-            1 - np.tanh(10 * goal_distance(grip_pos, object_pos))) * reach_mult
+        r_reach = (1 - np.tanh(10 * goal_distance(grip_pos, object_pos))) * reach_mult
 
         right_finger_layer = mujoco.mj_name2id(
-            self.model, mujoco.mjtObj.mjOBJ_GEOM, "right_finger_layer")
+            self.model, mujoco.mjtObj.mjOBJ_GEOM, "right_finger_layer"
+        )
         left_finger_layer = mujoco.mj_name2id(
-            self.model, mujoco.mjtObj.mjOBJ_GEOM, "left_finger_layer")
-        object_id = mujoco.mj_name2id(
-            self.model, mujoco.mjtObj.mjOBJ_GEOM, "object0")
-        r_grasp = (int(self._check_contact(right_finger_layer, object_id)
-                   or self._check_contact(left_finger_layer, object_id)) * grasp_mult)
+            self.model, mujoco.mjtObj.mjOBJ_GEOM, "left_finger_layer"
+        )
+        object_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_GEOM, "object0")
+        r_grasp = (
+            int(
+                self._check_contact(right_finger_layer, object_id)
+                or self._check_contact(left_finger_layer, object_id)
+            )
+            * grasp_mult
+        )
 
         r_lift = 0.0
-        if self._check_contact(right_finger_layer, object_id) and self._check_contact(left_finger_layer, object_id):
-            r_lift = grasp_mult + \
-                (1 - np.tanh(15.0 * goal_distance(target_pos, object_pos))) * \
-                (lift_mult - grasp_mult)
+        if self._check_contact(right_finger_layer, object_id) and self._check_contact(
+            left_finger_layer, object_id
+        ):
+            r_lift = grasp_mult + (
+                1 - np.tanh(15.0 * goal_distance(target_pos, object_pos))
+            ) * (lift_mult - grasp_mult)
 
         return r_reach, r_grasp, r_lift
 
     def _env_setup(self):
         if self.fetch_env:
-            if self.controller_type == 'mocap':
+            if self.controller_type == "mocap":
                 self._reset_mocap()
             else:
                 mujoco.mj_resetDataKeyframe(self.model, self.data, 0)
@@ -469,39 +486,53 @@ class MyCobotEnv(MujocoEnv):
 
         # Extract information for sampling goals.
         self.initial_gripper_xpos = mujoco_utils.get_site_xpos(
-            self.model, self.data, "EEF").copy()
+            self.model, self.data, "EEF"
+        ).copy()
 
         mujoco.mj_forward(self.model, self.data)
 
         self.height_offset = mujoco_utils.get_site_xpos(
-            self.model, self.data, "object0")[2]
+            self.model, self.data, "object0"
+        )[2]
 
         # Hide object in Reach env
         if not self.has_object:
             object_id = mujoco.mj_name2id(
-                self.model, mujoco.mjtObj.mjOBJ_GEOM, 'object0')
-            site_id = mujoco.mj_name2id(
-                self.model, mujoco.mjtObj.mjOBJ_SITE, 'object0')
+                self.model, mujoco.mjtObj.mjOBJ_GEOM, "object0"
+            )
+            site_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, "object0")
             self.model.geom_size[object_id] = np.zeros(3)
             self.model.site_size[site_id] = np.zeros(3)
 
-
     def _reset_mocap(self):
         mujoco_utils.set_mocap_quat(
-            self.model, self.data, "robot0:mocap", np.array([0.70809474, 0, -0.70611744, 0]))
+            self.model,
+            self.data,
+            "robot0:mocap",
+            np.array([0.70809474, 0, -0.70611744, 0]),
+        )
         mujoco.mj_step(self.model, self.data, nstep=self.frame_skip * 10)
         mujoco_utils.set_mocap_quat(
-            self.model, self.data, "robot0:mocap", np.array([0.50235287, -0.499, -0.5, 0.49764296]))
+            self.model,
+            self.data,
+            "robot0:mocap",
+            np.array([0.50235287, -0.499, -0.5, 0.49764296]),
+        )
         mujoco.mj_step(self.model, self.data, nstep=self.frame_skip * 10)
         mujoco_utils.set_mocap_pos(
-            self.model, self.data, "robot0:mocap", np.array([0.0138673, -0.13135342, 1.010216]))
+            self.model,
+            self.data,
+            "robot0:mocap",
+            np.array([0.0138673, -0.13135342, 1.010216]),
+        )
         mujoco.mj_step(self.model, self.data, nstep=self.frame_skip * 10)
 
-    def reset(self, * , seed: int | None = None,options: dict[str, Any] | None = None)-> tuple[ObsType, dict[str, Any]]:
+    def reset(
+        self, *, seed: int | None = None, options: dict[str, Any] | None = None
+    ) -> tuple[ObsType, dict[str, Any]]:
         if seed is not None:
             self._np_random, seed = seeding.np_random(seed)
         ob = self.reset_model()
         if self.render_mode == "human":
             self.render()
         return ob, {}
-    
